45f4c9afc26882bf11ca07580334d1506ca0b46c
Matheus Guilarducci <mguilarducci@gmail.com>
2015-04-21 11:21:18 -0300

    ordenar data de validade

--- api/controllers/ApiController.js
+++ api/controllers/ApiController.js
@@ -1,5 +1,84 @@
 var Machine = require("machine");
 module.exports = {
+    get_stock: function(req, res) {
+        Machine.build({
+            inputs: {},
+            exits: {
+                respond: {}
+            },
+            fn: function(inputs, exits) {
+                // Check login status
+                sails.machines['63fcae7b-edc4-4591-85b7-ba4863aa367e_0.2.3'].checkLogin({}).setEnvironment({
+                    req: req
+                }).exec({
+                    "error": function(checkLoginStatus) {
+                        return exits.error({
+                            data: checkLoginStatus,
+                            status: 500
+                        });
+
+                    },
+                    "otherwise": function(checkLoginStatus) {
+                        return exits.respond({
+                            data: "/signup",
+                            action: "redirect",
+                            status: 500
+                        });
+
+                    },
+                    "success": function(checkLoginStatus) {
+                        // List Stock
+                        sails.machines['_project_3032_0.0.1'].find_stock({
+                            "criteria": {
+                                userId: (req.session.me ? (req.session.me + '') : ''),
+                                count: {
+                                    ">": 0
+                                }
+                            }
+                        }).setEnvironment({
+                            sails: sails
+                        }).exec({
+                            "success": function(listStock) {
+                                // Sort
+                                sails.machines['0ccd2b47-a58e-4f8c-a3fd-d5a4ec77bfd5_4.4.0'].sort({
+                                    "collection": listStock,
+                                    "key": "expirationDate"
+                                }).exec({
+                                    "error": function(sort) {
+                                        return exits.error({
+                                            data: sort,
+                                            status: 500
+                                        });
+
+                                    },
+                                    "success": function(sort) {
+                                        return exits.respond({
+                                            data: sort,
+                                            action: "respond_with_result_and_status",
+                                            status: 200
+                                        });
+
+                                    }
+                                });
+
+                            },
+                            "error": function(listStock) {
+                                return exits.error({
+                                    data: listStock,
+                                    status: 500
+                                });
+
+                            }
+                        });
+
+                    }
+                });
+            }
+        }).configure(req.params.all(), {
+            respond: res.response,
+            error: res.negotiate
+        }).exec();
+    },
     post_stock: function(req, res) {
         Machine.build({
             inputs: {
@@ -256,66 +335,6 @@ module.exports = {
                                         });
 
                                     }
-                                });
-
-                            }
-                        });
-
-                    }
-                });
-            }
-        }).configure(req.params.all(), {
-            respond: res.response,
-            error: res.negotiate
-        }).exec();
-    },
-    get_stock: function(req, res) {
-        Machine.build({
-            inputs: {},
-            exits: {
-                respond: {}
-            },
-            fn: function(inputs, exits) {
-                // Check login status
-                sails.machines['63fcae7b-edc4-4591-85b7-ba4863aa367e_0.2.3'].checkLogin({}).setEnvironment({
-                    req: req
-                }).exec({
-                    "error": function(checkLoginStatus) {
-                        return exits.error({
-                            data: checkLoginStatus,
-                            status: 500
-                        });
-
-                    },
-                    "otherwise": function(checkLoginStatus) {
-                        return exits.respond({
-                            data: "/signup",
-                            action: "redirect",
-                            status: 500
-                        });
-
-                    },
-                    "success": function(checkLoginStatus) {
-                        // List Stock
-                        sails.machines['_project_3032_0.0.1'].find_stock({
-                            "criteria": {
-                                userId: (req.session.me ? (req.session.me + '') : '')
-                            }
-                        }).setEnvironment({
-                            sails: sails
-                        }).exec({
-                            "success": function(listStock) {
-                                return exits.respond({
-                                    data: listStock,
-                                    action: "respond_with_result_and_status",
-                                    status: 200
-                                });
-
-                            },
-                            "error": function(listStock) {
-                                return exits.error({
-                                    data: listStock,
-                                    status: 500
                                 });
 
                             }
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,8 @@
 module.exports.routes = {
+  "get /api/stock": "ApiController.get_stock",
   "post /signup": "SignupController.post_create",
   "post /api/stock": "ApiController.post_stock",
   "post /api/beers": "ApiController.post_beers",
-  "get /api/stock": "ApiController.get_stock",
   "get /": "Home$Controller.find",
   "post /login": "LoginController.create",
   "get /logout": "LogoutController.find",
